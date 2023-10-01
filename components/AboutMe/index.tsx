86b34a15eb4382ee885349e44d1be3db29264c14
Douglas Santos <douglas.crc@outlook.com>
2023-10-01 17:33:46 -0300

    adjusts

--- components/AboutMe/index.tsx
+++ components/AboutMe/index.tsx
@@ -35,7 +35,7 @@ const AboutMe = () => {
       <TranslateContainer>
         <span onClick={() => handleLocaleChange("pt")}>PT</span>
         <span> | </span>
-        <span onClick={() => handleLocaleChange("en")}>EN</span>
+        <span onClick={() => handleLocaleChange("en")}>EsN</span>
       </TranslateContainer>
       <AboutMeContainer>
       <DataAboutMe>
